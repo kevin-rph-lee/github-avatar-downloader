50e290341aa90c0b4a814dd53d0f8bd4118c912b
kevin <kevin.rph.lee@gmail.com>
2017-11-06 22:48:14 +0000

    adding additional functinality

--- download_avatars.js
+++ download_avatars.js
@@ -1,31 +1,38 @@
 var request = require('request');
 var secret = require('./secret');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb) {
+
+
+function getRepoContributors(repoOwner, repoName, callback) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
       'Authentication': 'token ' + secret
     }
-  };
-
-
-
+  }
   request(options, function(err, res, body) {
-    cb(err, JSON.parse(body));
+    callback(err, JSON.parse(body));
   });
+};
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .pipe(fs.createWriteStream(filePath));
 }
 
+
 getRepoContributors("jquery", "jquery", function(err, result) {
-
-
   for(var i = 0; i < result.length; i++){
-    console.log(result[i].avatar_url);
+    downloadImageByURL(result[i].avatar_url, './avatars/' + result[i].login);
   }
 });
+
+
 
 
 
