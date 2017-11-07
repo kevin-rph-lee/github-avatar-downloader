779896720902e1c66a1207ce1b36d6238d50dbcf
kevin <kevin.rph.lee@gmail.com>
2017-11-07 05:13:03 +0000

    fixed error messages and cleaned up error catching

--- download_avatars.js
+++ download_avatars.js
@@ -2,7 +2,7 @@
 const env = require('dotenv').config();
 //Error checking to see if the .env file is there
 if (env.error) {
-  console.log('Error! Env file not found!');
+  console.log('Error: Env file not found!');
   return;
 }
 
@@ -14,30 +14,33 @@ const args = process.argv;
  * Gets the repo contributors in an object and feeds it to a callback function
  * @param  {STRING}   repoOwner owner of the repo
  * @param  {STRING}   repoName  the name of the repo
+ * @param  {STRING}   accessToken  the profile access token for GitHub
  * @param  {Function} callback  function we will be calling on the contributors
  */
-function getRepoContributors(repoOwner, repoName, callback) {
+function getRepoContributors(repoOwner, repoName, accessToken, callback) {
   const options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token ' + process.env.GITHUB_API_TOKEN
+      'Authorization': 'token ' + accessToken
     }
   }
-
   //error checking conditions. Throws error message if caught.
-  if (args[2] === undefined || args[3] === undefined){
-    console.log('Error! Missing arguments!');
+  if (repoOwner === undefined || repoName === undefined){
+    console.log('Error: Missing arguments!');
   } else if (args.length > 4){
-    console.log('Too many parameters!');
-  } else if (process.env.GITHUB_API_TOKEN === undefined){
-    console.log('Error! Missing token in .env file!!');
+    console.log('Error: Too many parameters!');
+  } else if (accessToken === undefined){
+    console.log('Error: Missing token in .env file!');
   } else {
     request(options, function(err, res, body) {
       const bodyObj = JSON.parse(body);
+      //Additional error checking to see if the credentials are correct
       if(bodyObj['message'] === 'Bad credentials'){
-        console.log('Bad credentials!');
-      } else{
+        console.log('Error: Bad credentials!');
+      } else if(bodyObj['message'] === 'Not Found'){
+        console.log('Error: Repo not found!');
+      } else {
         callback(err, bodyObj);
       }
     });
@@ -56,7 +59,7 @@ function downloadImageByURL(url, filePath) {
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors(args[2], args[3], function(err, result) {
+getRepoContributors(args[2], args[3], process.env.GITHUB_API_TOKEN, function(err, result) {
     for(var i = 0; i < result.length; i++){
       downloadImageByURL(result[i].avatar_url, './avatars/' + result[i].login);
     }
