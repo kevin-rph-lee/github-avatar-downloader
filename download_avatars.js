538cb044a68c29d68b0b3fc01162c79feaba9151
kevin <kevin.rph.lee@gmail.com>
2017-11-07 02:50:15 +0000

    code change for more error checking

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,21 @@
-const env = require('dotenv').config();
 
+const env = require('dotenv').config();
+//Error checking to see if the .env file is there
+if (env.error) {
+  console.log('Error! Env file not found!');
+  return;
+}
 
 const request = require('request');
 const fs = require('fs');
-
-
 const args = process.argv;
 
+/**
+ * Gets the repo contributors in an object and feeds it to a callback function
+ * @param  {STRING}   repoOwner owner of the repo
+ * @param  {STRING}   repoName  the name of the repo
+ * @param  {Function} callback  function we will be calling on the contributors
+ */
 function getRepoContributors(repoOwner, repoName, callback) {
   const options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -16,22 +25,30 @@ function getRepoContributors(repoOwner, repoName, callback) {
     }
   }
 
+  //error checking conditions. Throws error message if caught.
   if (args[2] === undefined || args[3] === undefined){
     console.log('Error! Missing arguments!');
   } else if (args.length > 4){
     console.log('Too many parameters!');
   } else if (process.env.GITHUB_API_TOKEN === undefined){
     console.log('Error! Missing token in .env file!!');
-  } else if (result.message === 'Bad credentials'){
-    console.log('Bad credentials!');
   } else {
     request(options, function(err, res, body) {
-      callback(err, JSON.parse(body));
+      const bodyObj = JSON.parse(body);
+      if(bodyObj['message'] === 'Bad credentials'){
+        console.log('Bad credentials!');
+      } else{
+        callback(err, bodyObj);
+      }
     });
   }
 };
 
-
+/**
+ * downloads an image to a filepath
+ * @param  {string} url      where the file is located on the internet
+ * @param  {string} filePath where on the local machine to download the file
+ */
 function downloadImageByURL(url, filePath) {
   request.get(url)
     .pipe(fs.createWriteStream(filePath));
